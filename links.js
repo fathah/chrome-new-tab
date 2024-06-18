51d1b03a797d43a5c681668e94b73fb901096ffd
Fathah Cr <48355244+fathah@users.noreply.github.com>
2024-06-18 16:06:30 +0400

    Update links.js

--- links.js
+++ links.js
@@ -2,5 +2,6 @@ const LINKS = [
     {name:"Next.js Starter", link:"https://github.com/fathah/next-tailwind/"},
     {name:"Codeigniter Starter", link:"https://github.com/fathah/codegniter-starter"},
     {name:"React Icons", link:"https://react-icons.github.io/react-icons/search/"},
+ {name:"Material Icons", link:"https://fonts.google.com/icons"},
 
 ];
