2197088955e683b7d7d20d6cdabfbfae0eaa46e6
Fathah Cr <48355244+fathah@users.noreply.github.com>
2024-06-18 16:10:59 +0400

    Update links.js

--- links.js
+++ links.js
@@ -1,8 +1,8 @@
 const LINKS = [
-    {name:"Next.js Starter", link:"https://github.com/fathah/next-tailwind/"},
-    {name:"Codeigniter Starter", link:"https://github.com/fathah/codegniter-starter"},
-    {name:"React Icons", link:"https://react-icons.github.io/react-icons/search/"},
- {name:"Material Icons", link:"https://fonts.google.com/icons"},
- {name:"Line Icons", link:"https://icons8.com/line-awesome"},
+    {name:"Next.js Starter", link:"https://github.com/fathah/next-tailwind/", icon:"https://nextjs.org/favicon.ico"},
+    {name:"Codeigniter Starter", link:"https://github.com/fathah/codegniter-starter", icon:"https://codeigniter.com/favicons/apple-touch-icon.png"},
+    {name:"React Icons", link:"https://react-icons.github.io/react-icons/search/", icon:"https://react-icons.github.io/react-icons/favicon.png"},
+ {name:"Material Icons", link:"https://fonts.google.com/icons", icon:"https://www.gstatic.com/images/icons/material/apps/fonts/1x/catalog/v5/favicon.svg"},
+ {name:"Line Icons", link:"https://icons8.com/line-awesome", icon:"https://maxst.icons8.com/vue-static/landings/primary-landings/favs/icons8_fav_32%C3%9732.png"},
 
 ];
