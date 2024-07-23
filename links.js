58c4b370b6dcd26798014ee15f36d5d8dd54ee3d
Fathah KA <48355244+fathah@users.noreply.github.com>
2024-07-23 16:45:11 +0400

    Update links.js

--- links.js
+++ links.js
@@ -4,5 +4,7 @@ const LINKS = [
     {name:"React Icons", link:"https://react-icons.github.io/react-icons/search/", icon:"https://react-icons.github.io/react-icons/favicon.png"},
  {name:"Material Icons", link:"https://fonts.google.com/icons", icon:"https://www.gstatic.com/images/icons/material/apps/fonts/1x/catalog/v5/favicon.svg"},
  {name:"Line Icons", link:"https://icons8.com/line-awesome", icon:"https://maxst.icons8.com/vue-static/landings/primary-landings/favs/icons8_fav_32%C3%9732.png"},
+ {name:"CI4 Transaction", link:"https://codeigniter.com/user_guide/database/transactions.html", icon:"https://codeigniter.com/favicons/apple-touch-icon.png"},
+     {name:"Animated Gradient", link:"https://animated-gradient-background-generator.netlify.app/", icon:"https://images.ctfassets.net/h6goo9gw1hh6/2VwHU3zc2pQLXBRA7nxOoL/4a6608bbc21f485609aee930cb76e2e2/Gradient_Background_Example.jpg"},
 
 ];
