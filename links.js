2622c079b88175f25464a5e3548243bb0ae49fe9
Fathah KA <48355244+fathah@users.noreply.github.com>
2024-11-07 16:27:16 +0400

    Update links.js

--- links.js
+++ links.js
@@ -1,6 +1,6 @@
 const LINKS = [
     {name:"Roadmap.sh", link:"https://roadmap.sh/", icon:"https://roadmap.sh/manifest/icon32.png"},
-    {name:"Next.js Starter", link:"https://github.com/fathah/next-tailwind/", icon:"https://nextjs.org/favicon.ico"},
+    {name:"Project Starters", link:"https://github.com/fathah/project-starters/", icon:"https://nextjs.org/favicon.ico"},
     {name:"Codeigniter Starter", link:"https://github.com/fathah/codegniter-starter", icon:"https://codeigniter.com/favicons/apple-touch-icon.png"},
     {name:"React Icons", link:"https://react-icons.github.io/react-icons/search/", icon:"https://react-icons.github.io/react-icons/favicon.png"},
  {name:"Material Icons", link:"https://fonts.google.com/icons", icon:"https://www.gstatic.com/images/icons/material/apps/fonts/1x/catalog/v5/favicon.svg"},
