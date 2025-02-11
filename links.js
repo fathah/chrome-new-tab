d0de2f4a991b09cc9fc9c41dbc942d70a5549994
fathaaah⚡️ <48355244+fathah@users.noreply.github.com>
2025-02-11 10:25:33 +0400

    Update links.js

--- links.js
+++ links.js
@@ -19,6 +19,8 @@ const LINKS = [
  {name:"MerakiUI", link:"https://merakiui.com/", icon:"https://merakiui.com/images/logo.svg"},
      {name:"RX DB", link:"https://rxdb.info", icon:"https://rxdb.info/files/logo/logo.svg"},
      {name:"Mantine", link:"https://mantine.dev/", icon:"https://mantine.dev/favicon.svg"},
+         {name:"OpenRouter AI API", link:"https://openrouter.ai/", icon:"https://openrouter.ai/favicon.ico"},
+
 
 
 ];
