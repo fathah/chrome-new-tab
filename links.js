1e3b3b5a9aab2dc0bb7e7b33018b9b3fbfbef5bd
fathaaah⚡️ <48355244+fathah@users.noreply.github.com>
2025-08-01 18:00:45 +0400

    Update links.js

--- links.js
+++ links.js
@@ -23,6 +23,9 @@ const LINKS = [
          {name:"GeoJS API", link:"https://get.geojs.io/v1/ip/geo.json", icon:"https://www.geojs.io/favicon/v1/apple-touch-icon.png"},
              {name:"Glass CSS", link:"https://glass3d.dev/?utm_source=ziqx.cc", icon:"https://www.geojs.io/favicon/v1/a"},
                  {name:"Transparent Textures", link:"https://transparenttextures.com/?utm_source=ziqx.cc", icon:"https://transparenttextures.com/favicon.png"},
+                 {name:"Iconify", link:"https://iconify.design/?utm_source=ziqx.cc", icon:"https://iconify.design/favicon.svg"},
+
+   
 
 
     
