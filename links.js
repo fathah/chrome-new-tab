d9647f7764e771e37e402954cef9ee2616846f5e
fathaaah⚡️ <48355244+fathah@users.noreply.github.com>
2025-11-21 10:48:59 +0400

    Update icons for Project Starters and NIX OS links

--- links.js
+++ links.js
@@ -1,14 +1,11 @@
 const LINKS = [
-    {name:"Project Starters", link:"https://github.com/fathah/project-starters/", icon:"https://nextjs.org/favicon.ico"},
+    {name:"Project Starters", link:"https://github.com/fathah/project-starters/", icon:"https://github.com/fluidicon.png"},
     {name:"Roadmap.sh", link:"https://roadmap.sh/", icon:"https://roadmap.sh/manifest/icon32.png"},
-    
-    {name:"Codeigniter Starter", link:"https://github.com/fathah/codegniter-starter", icon:"https://codeigniter.com/favicons/apple-touch-icon.png"},
-    {name:"React Icons", link:"https://react-icons.github.io/react-icons/search/", icon:"https://react-icons.github.io/react-icons/favicon.png"},
+        {name:"React Icons", link:"https://react-icons.github.io/react-icons/search/", icon:"https://react-icons.github.io/react-icons/favicon.png"},
  {name:"Material Icons", link:"https://fonts.google.com/icons", icon:"https://www.gstatic.com/images/icons/material/apps/fonts/1x/catalog/v5/favicon.svg"},
  {name:"Line Icons", link:"https://icons8.com/line-awesome", icon:"https://maxst.icons8.com/vue-static/landings/primary-landings/favs/icons8_fav_32%C3%9732.png"},
- {name:"CI4 Transaction", link:"https://codeigniter.com/user_guide/database/transactions.html", icon:"https://codeigniter.com/favicons/apple-touch-icon.png"},
      {name:"Animated Gradient", link:"https://animated-gradient-background-generator.netlify.app/", icon:"https://images.ctfassets.net/h6goo9gw1hh6/2VwHU3zc2pQLXBRA7nxOoL/4a6608bbc21f485609aee930cb76e2e2/Gradient_Background_Example.jpg"},
-    {name:"NIX OS", link:"https://nixos.org/", icon:"https://nixos.org/_astro/flake-blue.Bf2X2kC4_Z1yqDoT.svg"},
+    {name:"NIX OS", link:"https://nixos.org/", icon:"https://nixos.org/apple-touch-icon.png"},
     {name:"Search Console", link:"https://search.google.com/search-console", icon:"https://www.gstatic.com/search-console/scfe/search_console-128.png"},
     {name:"Open Food Facts", link:"https://world.openfoodfacts.org/", icon:"https://world.openfoodfacts.org/images/favicon/favicon-16x16.png"},
     {name:"Public APIs", link:"https://www.freepublicapis.com/?utm_source=ziqx.cc", icon:"https://static.vecteezy.com/system/resources/previews/029/345/984/original/api-icon-data-analytics-monitoring-setting-gear-database-icons-free-png.png"},
